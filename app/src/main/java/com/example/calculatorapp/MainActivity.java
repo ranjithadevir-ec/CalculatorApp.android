6fcef4f3983587aa8988667d69262ed72be1d31d
ranjithadevir-ec <ramuranjitha20@gmail.com>
2026-06-15 14:11:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.calculatorapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText num1, num2;
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView txtResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        txtResult = findViewById(R.id.txtResult);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        btnSub = findViewById(R.id.btnSub);
+        btnMul = findViewById(R.id.btnMul);
+        btnDiv = findViewById(R.id.btnDiv);
+
+        btnAdd.setOnClickListener(v -> calculate("+"));
+        btnSub.setOnClickListener(v -> calculate("-"));
+        btnMul.setOnClickListener(v -> calculate("*"));
+        btnDiv.setOnClickListener(v -> calculate("/"));
+    }
+
+    private void calculate(String op) {
+
+        String a1 = num1.getText().toString();
+        String a2 = num2.getText().toString();
+
+        if (a1.isEmpty() || a2.isEmpty()) {
+            txtResult.setText("Enter numbers");
+            return;
+        }
+
+        double a = Double.parseDouble(a1);
+        double b = Double.parseDouble(a2);
+        double result = 0;
+
+        switch (op) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                if (b == 0) {
+                    txtResult.setText("Cannot divide by zero");
+                    return;
+                }
+                result = a / b;
+                break;
+        }
+
+        txtResult.setText("Result: " + result);
+    }
+}
